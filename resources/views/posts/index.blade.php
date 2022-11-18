b6e9c362a2ab3c7a12a1bc67c993a031b9cbd709
youhei0423 <ootayouhei_0423@icloud.com>
2022-11-18 12:36:38 +0900

    dev_basis03

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,8 +11,8 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
+                    <a href="/posts/{{$post->id}}"><h2 class="title">{{$post->title}}</h2></a>
+                    <p class="body">{{$post->body}}</p>
                 </div>
             @endforeach
         </div>
